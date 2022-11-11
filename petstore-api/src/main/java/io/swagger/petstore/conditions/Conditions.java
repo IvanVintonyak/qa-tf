b50a4329b4c40418fff7b4ae342b202afd73c427
Andrii <avoevodin81@gmail.com>
2022-11-11 18:58:55 +0200

    petstore-api basic implementation

---
+++ petstore-api/src/main/java/io/swagger/petstore/conditions/Conditions.java
@@ -0,0 +1,16 @@
+package io.swagger.petstore.conditions;
+
+import lombok.experimental.UtilityClass;
+import org.hamcrest.Matcher;
+
+@UtilityClass
+public class Conditions {
+
+    public StatusCodeCondition statusCode(int code) {
+        return new StatusCodeCondition(code);
+    }
+
+    public BodyFieldCondition bodyField(String jsonPath, Matcher<String> matcher) {
+        return new BodyFieldCondition(jsonPath, matcher);
+    }
+}
